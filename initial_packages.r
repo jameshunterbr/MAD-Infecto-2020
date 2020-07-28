a0cefbd3855f0fe9832f3e7e86bc058b1e29265f
James Hunter <jameshunterbr@gmail.com>
2020-07-28 18:17:47 -0300

    initial_packages
    Initial_packages function to be downloaded by students

---
+++ initial_packages.r
@@ -0,0 +1,14 @@
+### Function to Install Initial Packages for MAD-Infecto-2020 ###
+### James Hunter
+### 28/7/20
+
+packages <- c("tidyverse", "broom", "car", "caret", "corrr", "data.table", 
+              "descr", "devtools", "gapminder", "ggpubr", "ggvis", "ggsci", 
+              "glue", "gmodels", "gt", "here", "Hmisc", "hms", "janitor", 
+              "jsonlite", "kableExtra","knitr", "lattice", "librarian", 
+              "lubridate", "magrittr", "mice", "nortest", "nycflights13", 
+              "outliers", "palmerpenguins", "pROC", "psych", "RColorBrewer", 
+              "Rcpp", "readxl", "ROCR", "shiny", "styler", "summarytools", 
+              "usethis", "titanic")
+
+install.packages(packages)
